55e755640ca4ebf02176c31626e5ae70bb1b250d
Sergey <kopgamerk@gmail.com>
2022-03-17 08:08:53 +0300

    Initial Commit

---
+++ HomeWork3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork3
+//
+//  Created by u on 17.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
