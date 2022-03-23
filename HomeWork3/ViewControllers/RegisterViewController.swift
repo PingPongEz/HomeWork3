41d53fd65f5758e4a149908d2c774401e8158018
Sergey <kopgamerk@gmail.com>
2022-03-23 12:49:44 +0300

    3

--- HomeWork3/ViewControllers/RegisterViewController.swift
+++ HomeWork3/ViewControllers/RegisterViewController.swift
@@ -9,8 +9,13 @@ import UIKit
 
 class RegisterViewController: UIViewController {
     
-    @IBOutlet private var registerPasswordTF: UITextField!
     @IBOutlet private var registerUsernameTF: UITextField!
+    @IBOutlet private var registerPasswordTF: UITextField!
+
+    override func viewDidLoad() {
+        registerUsernameTF.delegate = self
+        registerPasswordTF.delegate = self
+    }
 
     @IBAction private func registerDonePressed() {
         
@@ -21,10 +26,32 @@ class RegisterViewController: UIViewController {
         if !newUsername.isEmpty && !newPassword.isEmpty {
             
             usersInApp.addUserWithName(newUsername, andPass: newPassword)
+            print(usersInApp.arrayOfUsers)
             
         } else {
             print("You can't register empty name or password")
+            return
         }
         dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+//MARK: Work with keyboard and touches
+extension RegisterViewController: UITextFieldDelegate {
+    
+    override func touchesBegan(_ touches: Set<UITouch>,
+                               with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == registerUsernameTF {
+            registerPasswordTF.becomeFirstResponder()
+        } else {
+            registerDonePressed()
+        }
+        return true
     }
 }
